b689480070e8f22e94cd5503e516e74cafcfce94
Maxime Hadjinlian <maxime.hadjinlian@devialet.com>
2014-01-16 14:20:23 +0100

    Add setup.py
    Add an installer, to install grabserial, simply do:
    python setup.py install
    
    This could help to put this into Pypi, or to create a Debian packages.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@devialet.com>

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup
+
+VERSION = '1.6.0'
+
+setup(
+    name='grabserial',
+    version=VERSION,
+    scripts=['grabserial',],
+    author='Tim Bird',
+    author_email='tbird20d@yahoo.com',
+
+    maintainer='Tim Bird',
+    maintainer_email='tbird20d@yahoo.com',
+
+    description='Serial dump and timing program',
+    long_description='''
+grabserial is a small program which reads a serial port and writes the data
+to standard output. The main purpose of this tool is to collect messages
+written to the serial console from a target board running Linux, and save
+the messages on a host machine.
+''',
+    url='http://github.com/tbird20d/grabserial',
+    license='GPL v2',
+    keywords='grabserial serial boot time optimization tool',
+    classifiers=[
+        "Topic :: Utilities",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Embedded Systems",
+    ],
+
+    install_requires=[
+        "pyserial>=2.6"
+    ],
+)
